35db353322d59a430a898fb29cf7fa2086b3b8f5
Sergio <18sgdc@gmail.com>
2021-02-24 16:01:26 +0100

    Nueva atualizacion patrocinada por Sergio S.L.

--- PruebaGit/app/src/main/java/com/example/pruebagit/MainActivity.kt
+++ PruebaGit/app/src/main/java/com/example/pruebagit/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var count: Int = 10
+        var count: Int = 10000
+        var count2: Int = 0
     }
 }
